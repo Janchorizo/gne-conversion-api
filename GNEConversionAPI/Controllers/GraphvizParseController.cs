9aa55bd5acc00b79a0f3f1f95af3bd6de054d552
Alejandro Rodríguez Díaz <id00681749@usal.es>
2020-09-21 01:19:31 +0200

    Add Graphviz svg parse controller.

---
+++ GNEConversionAPI/Controllers/GraphvizParseController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using GNEConversionAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GNEConversionAPI.Controllers
+{
+    [Route("parse")]
+    [ApiController]
+    public class GraphvizParseController : ControllerBase
+    {
+        [HttpPost]
+        public string Get()
+        {
+            try
+            {
+                string nodeString = HttpContext.Request.Form["node"];
+                var node = JsonSerializer.Deserialize<SVGNodeDescription>(
+                    nodeString,
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                string linkString = HttpContext.Request.Form["link"];
+                var link = JsonSerializer.Deserialize<SVGNodeDescription>(
+                    linkString,
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                return node.Xpath + link.Xpath;
+            }
+            catch (Exception e)
+            {
+                var status = new Dictionary<string, bool>(){
+                    { "error", true }
+                };
+                var json = JsonSerializer.Serialize<Dictionary<string, bool>>(status);
+                return json;
+            }
+        }
+    }
+}
