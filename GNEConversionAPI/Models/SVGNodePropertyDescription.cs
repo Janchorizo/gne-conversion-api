3accb02ca764f4a65c1da59f716ada175dbf59cf
Alejandro Rodríguez Díaz <id00681749@usal.es>
2020-09-21 01:19:07 +0200

    Create SVGNodePropertyDescription model.

---
+++ GNEConversionAPI/Models/SVGNodePropertyDescription.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace GNEConversionAPI.Models
+{
+    public class SVGNodePropertyDescription
+    {
+  
+        public string Name { get; set;  }
+        public string Xpath { get; set;  }
+        public string Regex { get; set;  }
+    }
+}
