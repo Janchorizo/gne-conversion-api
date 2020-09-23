117383ec033e3a68be525869458102b9603de77b
Alejandro Rodríguez Díaz <id00681749@usal.es>
2020-09-23 08:53:56 +0200

    Add attributes, setters and empty methods to SVGNodeParser.

--- GNEConversionAPI/Services/Parsing/SVGNodeParser.cs
+++ GNEConversionAPI/Services/Parsing/SVGNodeParser.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using GNEConversionAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace GNEConversionAPI.Services.Parsing
 {
     public class SVGNodeParser
     {
+        private string Xpath;
+        private IEnumerable<SVGNodePropertyDescription> Properties;
+        private IEnumerable<string> CompulsoryProperties;
+        public void SetXpath(string xpath) { }
+        public void SetProperties(IEnumerable<SVGNodePropertyDescription> properties) { }
+        public void SetCompulsoryProperties(IEnumerable<string> compulsoryPropertyNames) { }
+        public IEnumerable<SVGNode> ParseAll(string svg) { }
+        private Dictionary<string, string> GetProperties(XMLNode node) { }
+        private string GetProperty(XmlNode node, SVGNodePropertyDescription property) { }
+        private IEnumerable<SVGNode> GetNodes(string svg) { }
     }
 }
