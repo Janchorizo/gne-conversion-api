912bbb13a24cc9ca8fd52aa8b03bd63f2c46228e
Alejandro Rodríguez Díaz <id00681749@usal.es>
2020-09-23 09:28:50 +0200

    Add builder interface methods to concrete SVGNodeParserBuilders.

--- GNEConversionAPI/Services/Parsing/NetworkNodeParserBuilder.cs
+++ GNEConversionAPI/Services/Parsing/NetworkNodeParserBuilder.cs
@@ -1,11 +1,37 @@
-﻿using System;
+﻿using GNEConversionAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace GNEConversionAPI.Services.Parsing
 {
-    public class NetworkNodeParserBuilder
+    public class NetworkNodeParserBuilder : ISVGNodeParserBuilder
     {
+        private SVGNodeParser Result;
+        SVGNodeParser ISVGNodeParserBuilder.getResult()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ISVGNodeParserBuilder.Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ISVGNodeParserBuilder.SetCompulsoryProperties()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ISVGNodeParserBuilder.SetNodeXpath(string xpath)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ISVGNodeParserBuilder.SetProperties(IEnumerable<SVGNodePropertyDescription> )
+        {
+            throw new NotImplementedException();
+        }
     }
 }
