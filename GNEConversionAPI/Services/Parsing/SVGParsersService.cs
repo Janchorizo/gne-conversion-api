ce8d67daedfcf795e58cc720808c0ecafeea8421
Alejandro Rodríguez Díaz <id00681749@usal.es>
2020-09-23 12:02:23 +0200

    Implement service for svg parser creation.

--- GNEConversionAPI/Services/Parsing/SVGParsersService.cs
+++ GNEConversionAPI/Services/Parsing/SVGParsersService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GNEConversionAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,24 @@ namespace GNEConversionAPI.Services.Parsing
 {
     public class SVGParsersService
     {
+        private NodeParserDirector Director;
+        public SVGParsersService() {
+            var initialBuilder = new NetworkNodeParserBuilder();
+            this.Director = new NodeParserDirector(initialBuilder);
+        }
+        public SVGNodeParser GetNodeParser(SVGNodeDescription target)
+        {
+            ISVGNodeParserBuilder nodeParserBuilder = new NetworkNodeParserBuilder();
+            this.Director.ChangeBuilder(nodeParserBuilder);
+            this.Director.Make(target);
+            return nodeParserBuilder.GetResult();
+        }
+        public SVGNodeParser GetLinkParser(SVGNodeDescription target)
+        {
+            ISVGNodeParserBuilder linkParserBuilder = new NetworkLinkParserBuilder();
+            this.Director.ChangeBuilder(linkParserBuilder);
+            this.Director.Make(target);
+            return linkParserBuilder.GetResult();
+        }
     }
 }
