1040126fd33f66453fe6cfdeeef06144cc88908e
Alejandro Rodríguez Díaz <id00681749@usal.es>
2020-09-23 09:27:29 +0200

    Implement NodeParserDirector.

--- GNEConversionAPI/Services/Parsing/NodeParserDirector.cs
+++ GNEConversionAPI/Services/Parsing/NodeParserDirector.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GNEConversionAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,21 @@ namespace GNEConversionAPI.Services.Parsing
 {
     public class NodeParserDirector
     {
+        private ISVGNodeParserBuilder Builder;
+        public NodeParserDirector(ISVGNodeParserBuilder builder)
+        {
+            this.Builder = builder;
+        }
+        public void ChangeBuilder(ISVGNodeParserBuilder builder)
+        {
+            this.Builder = builder;
+        }
+        public void Make(SVGNodeDescription nodeDescription)
+        {
+            this.Builder.Reset();
+            this.Builder.SetNodeXpath(nodeDescription.Xpath);
+            this.Builder.SetProperties(nodeDescription.Properties);
+            this.Builder.SetCompulsoryProperties();
+        }
     }
 }
