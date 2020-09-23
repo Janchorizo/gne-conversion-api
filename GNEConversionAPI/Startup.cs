e12676ca1bcf55d609a4d9216580c2d9a1bd4eca
Alejandro Rodríguez Díaz <id00681749@usal.es>
2020-09-23 17:55:56 +0200

    Allow CORS for local develpoment and github pages.

--- GNEConversionAPI/Startup.cs
+++ GNEConversionAPI/Startup.cs
@@ -17,6 +17,7 @@ namespace GNEConversionAPI
 {
     public class Startup
     {
+        readonly string MyAllowSpecificOrigins = "_MyAllowSpecificOrigins";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -27,6 +28,14 @@ namespace GNEConversionAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  builder =>
+                                  {
+                                      builder.WithOrigins("http://localhost:3000", "https://janchorizo.github.io");
+                                  });
+            });
             services.AddControllers();
             services.AddSingleton<SVGParsersService>();
             services.AddSingleton<GraphvizService>();
@@ -39,6 +48,8 @@ namespace GNEConversionAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseHttpsRedirection();
 
